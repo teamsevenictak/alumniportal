733aa60137417de4378eaf9ae7324562899f48aa
teamsevenictak <109238460+teamsevenictak@users.noreply.github.com>
2022-07-23 12:01:04 +0530

    Add files via upload

--- Backend/src/model/PostjobsModel.js
+++ Backend/src/model/PostjobsModel.js
@@ -1,12 +1,15 @@
 const mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost:27017/AlumniPortal');
+//mongoose.connect('mongodb://localhost:27017/AlumniPortal');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 const Schema = mongoose.Schema;
 
 const Postajob = new Schema ({
     companyName: String,
-    jobRole: String, 
+    jobRole: String,
+    companydetail:String,
+    jobcategory:String,
     location: String,
-    experience: String,
+    experience: Number,
     skills: String,
     qualification: String,
     jobDescription: String,
