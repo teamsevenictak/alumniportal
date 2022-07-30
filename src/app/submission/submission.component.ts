a8271e3b22b4cf39fdbffc562d2608419798ded8
teamsevenictak <109238460+teamsevenictak@users.noreply.github.com>
2022-07-30 10:01:55 +0530

    Add files via upload

--- src/app/submission/submission.component.ts
+++ src/app/submission/submission.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { JobpostingService } from '../jobposting.service';
 
 @Component({
   selector: 'app-submission',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmissionComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public jobpostingService: JobpostingService,public _auth:AuthService,private router:Router) { }
+  application  = 
+  { name : '',
+  filelink     : '',
+  postID   :'',
+  AlumnId  :'',
+  Dateofsub:'',
+  visibility:''
+ }
   ngOnInit(): void {
+    this.jobpostingService.getApplications().subscribe((data)=>{
+      this.application = JSON.parse(JSON.stringify(data));
+    })
   }
+  sendToEmployer(application:any){
+    var  AppId = application._id;
+    this.jobpostingService.updateById(AppId).subscribe((data)=>{
+         // this.router.navigate(['/jobdetail']); 
+    })
+   }
 
 }
