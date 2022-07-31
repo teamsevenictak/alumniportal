42d0e25ec2dc3acd27a1a5dcc31d7ff713bbd542
teamsevenictak <109238460+teamsevenictak@users.noreply.github.com>
2022-08-01 00:32:36 +0530

    Add files via upload

--- src/app/jobposting.service.ts
+++ src/app/jobposting.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient, HttpParams } from '@angular/common/http'; 
 import { Observable, Subscription } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -14,11 +14,15 @@ export class JobpostingService {
   uploadProgress :Number=0;
   uploadSub!: Subscription;
   selectedId: string ='';
+  emplyId   : string ='';
 
   constructor(public http: HttpClient,private _auth:AuthService) { }
 
   getJobs(){
     return this.http.get(`${this.server_address}/postajob`)
+  }
+  getlatestJobs(){
+    return this.http.get(`${this.server_address}/latestjobs`)
   }
  getJobsByUser(){
   return this.http.get(`${this.server_address}/postsbyuser`);
@@ -26,18 +30,27 @@ export class JobpostingService {
   addJobs(item:any){
     this.userId  = this._auth.getLoggedUserID();
     item.userId =  this.userId;
-    console.log(item);
     return this.http.post(`${this.server_address}/addJob`,{item});
     
   }
   getJobById(JobId: any){
+    this.selectedId = JobId;
     return this.http.get(`${this.server_address}/jobdetail/`+JobId);
   }
+  getApplicationById(item: { postID: string; AlumnId: any; }){ 
+    let queryParams = new HttpParams().append("postID",item.postID)
+                                      .append("AlumnId",item.AlumnId);
+    return this.http.get(`${this.server_address}/getAppById/`,{params:queryParams});
+  }
+  
   getApplications(){
     return this.http.get(`${this.server_address}/getapplicatins`);
   }
   updateById(Appid:any){
     return this.http.put(`${this.server_address}/updateapplicatin`,{Appid:Appid});
+  }
+  verifyById(PostId:any){
+    return this.http.put(`${this.server_address}/verifypost`,{PostId:PostId});
   }
   upload(file:File,application:any)  {
     var formData = new FormData();
@@ -45,8 +58,9 @@ export class JobpostingService {
     formData.append("name", application.name);
     formData.append("filelink", application.filelink);
     formData.append("alumniID",  this._auth.getLoggedUserID());
-    //var  jobId = localStorage.getItem('jobID');
-    formData.append("jobId", this.selectedId);
+    var  jobId = localStorage.getItem('jobID');    
+    formData.append("postID", this.selectedId);
+    formData.append("emplyId", this.emplyId);
     return this.http.post(`${this.server_address}/resume-upload`, formData, {
       reportProgress: true,
       observe: 'events'
@@ -55,6 +69,15 @@ export class JobpostingService {
       //finalize(() => this.reset())
   );
     
+  }
+  submitapplication(application:any){
+    var formData = new FormData();
+    formData.append("name", application.name);
+    formData.append("filelink", application.filelink);
+    formData.append("alumniID",  this._auth.getLoggedUserID());
+    var  jobId = localStorage.getItem('jobID');
+    formData.append("postID", this.selectedId);
+    return this.http.post(`${this.server_address}/resume-submit`, formData)
   }
   cancelUpload() {
     this.uploadSub.unsubscribe();
